9902461f5c3c13156ac734b4cc34f79d86d10700
Nsuliman <nsuliman88@gmail.com>
2020-03-05 15:23:37 +0200

    app.js and modules added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
 import React from 'react';
+import Footer from './modules/Footer.js';
+import Header from './modules/Header.js';
+
+import './app.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Footer />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/modules/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// dummy component - it is also a functional component
+const Header = () => {
+  return (
+    <header className="counter-app">
+      <h1> Props And State </h1>
+    </header>
+  );
+};
+
+export default Header;
