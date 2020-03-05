bb3dacd69b1a6eb530f10f18eb49f3cb778d51d3
Nsuliman <nsuliman88@gmail.com>
2020-03-05 18:04:12 +0200

    scalfold for form done

--- src/components/resty.js
+++ src/components/resty.js
@@ -5,7 +5,7 @@ class Resty extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            url : ''
+            url: ''
         } // end of state
     } // end of constructor 
 
@@ -16,33 +16,45 @@ class Resty extends React.Component {
         this.setState({ url });
     }
 
-   
+
     render() {
-        return(
+        return (
             <>
-            <Urlfun url1={this.state.url}/>
-            <Form handler={this.handleURL}/>
+                <Urlfun url1={this.state.url} />
+                <Form handler={this.handleURL} />
             </>
         );
 
     } // end of render method 
 
-    
+
 
 } // end of resty class 
 
 
 function Urlfun({ url1 }) {
-return <h1> URL Full Address : {url1}</h1>
+    return <h1> URL Full Address : {url1}</h1>
 }
 
 function Form(props) {
-    return(
+    return (
         <form>
-            <label> URL :  
+            <section>
+            <label className="apiC"> API URL :
             <input type='text' onChange={props.handler} />
             </label>
+            </section>
+            <section className="methodC">
+                <label> GET</label>
+                <label> POST</label>
+                <label> PUT</label>
+                <label> DELETE</label>
+                <label> PATCH</label>
+                <button> GO!</button>
+            </section>
+            <textarea></textarea>
         </form>
+
     );
 }
 
