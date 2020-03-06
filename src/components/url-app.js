6bc3921f2fc85b6ed3b1a3ccae7b988343741bda
Nsuliman <nsuliman88@gmail.com>
2020-03-06 22:48:57 +0200

    UML added , lab done for get only , eslint errors fixed expect 4 errors , that's all

--- src/components/url-app.js
+++ src/components/url-app.js
@@ -3,88 +3,83 @@ import superagent from 'superagent';
 
 
 class Resty extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            url: '',
-            restMod: '',
-            results: []
-            // headers : {},
-            // body : {}
-        } // end of state object
-    } // end of constructor 
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: '',
+      restMod: '',
+      results: [],
+      headers : {},
+      body : {}
+    }; // end of state object
+  } // end of constructor 
 
     submitRes = async (event) => {
-        event.preventDefault();
-        console.log('this.state.url : ', this.state.url);
-        superagent.get(`${this.state.url}`)
-            .then(data => {
-                console.log('data results', data.body.results)
-                console.log('data headers', data.header)
-                let res = data.body.results;
-                this.setState({ results: res });
-            })
+      event.preventDefault();
+      console.log('this.state.url : ', this.state.url);
+      superagent.get(`${this.state.url}`)
+        .then(data => {
+          console.log('data results', data.body.results);
+          console.log('data headers', data.header);
+          let res = data.body.results;
+          this.setState({ results: res });
+        });
     } // end of submitRes event 
 
 
 
     handleURL = event => {
-        event.preventDefault();
-        let url = event.target.value;
-        console.log('urlEvent : ', url);
-        this.setState({ url });
+      event.preventDefault();
+      let url = event.target.value;
+      console.log('urlEvent : ', url);
+      this.setState({ url });
     } // end of handleURL event 
 
     handleGet = event => {
-        event.preventDefault();
-        let getMethod = event.target.value;
-        console.log('getMethod : ', getMethod);
-        this.setState({ getMethod });
+      event.preventDefault();
+      let getMethod = event.target.value;
+      console.log('getMethod : ', getMethod);
+      this.setState({ getMethod });
     } // end of handleGet event 
     render() {
-        return (
+      return (
             <>
                 <Urlfun url1={this.state.url} />
-                <Form handler={this.handleURL} gethandler={this.handleGet} handleSubmit={this.submitRes} res1={this.state.results}/>
-                {/* <Print  /> */}
+                <Form handler={this.handleURL} gethandler={this.handleGet} handleSubmit={this.submitRes} res1={this.state.results} />
             </>
-        );
-
+      );
     } // end of render method 
-
-
-
 } // end of resty class 
 
 function Print({ res1 }) {
-    console.log('res1 : ', res1);
-    return <p> ppppppppppppppp {res1}</p>
+  console.log('res1 : ', res1);
+  return <p> ppppppppppppppp {res1}</p>;
 }
 
 function Urlfun({ url1 }) {
-    return <h1> URL Full Address : {url1}</h1>
+  return <h1> URL Full Address : {url1}</h1>;
 }
 
 function Form(props) {
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <section>
-                <label className="apiC"> API URL :
-            <input type='text' onChange={props.handler} />
-                </label>
-            </section>
-            <section className="methodC">
-                <button className="methods" onClick={props.gethandler} value="get"> GET</button>
-                <button className="methods" onClick={props.gethandler} value="post"> POST</button>
-                <button className="methods" onClick={props.gethandler} value="put"> PUT</button>
-                <button className="methods" onClick={props.gethandler} value="patch"> PATCH</button>
-                <button className="methods" onClick={props.gethandler} value="delete"> DELETE</button>
-                <button className="submitB"> GO!</button>
-            </section>
-            <p> The Results </p>
-            <textarea type="text">props.results</textarea>
-        </form>
-    );
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <section>
+        <label className="apiC"> API URL :
+          <input type='text' onChange={props.handler} />
+        </label>
+      </section>
+      <section className="methodC">
+        <button className="methods" onClick={props.gethandler} value="get"> GET</button>
+        <button className="methods" onClick={props.gethandler} value="post"> POST</button>
+        <button className="methods" onClick={props.gethandler} value="put"> PUT</button>
+        <button className="methods" onClick={props.gethandler} value="patch"> PATCH</button>
+        <button className="methods" onClick={props.gethandler} value="delete"> DELETE</button>
+        <button className="submitB"> GO!</button>
+      </section>
+      <p> The Results </p>
+      <textarea type="text"></textarea>
+    </form>
+  );
 } // end of Form function 
 
 
